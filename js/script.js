933d3f29cd3753f79403947883bb35b1d3b7b246
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2022-10-04 21:44:39 +0200

    Formatted code

--- js/script.js
+++ js/script.js
@@ -12,124 +12,118 @@ const navMainMenu = document.querySelector('.navigation__main');
 const navSubMenu = document.querySelectorAll('.navigation__category');
 const headerLogo = document.querySelector('.header__logo-container');
 
-
+// Variables
 let mainNavigationOpen = false;
 let subNavigationOpen = false;
+const delayInMilliseconds = 400;
 
 // Initialization 
-init();
+MobileNavigationInit();
 
-function init () {
-  hideSubMenus();
-  
-  navMainMenu.classList.remove('hide')
-  backButton.classList.remove('header__back-button--open');
+function MobileNavigationInit () {
+	hideSubMenus();
+
+	navMainMenu.classList.remove('hide')
+	backButton.classList.remove('header__back-button--open');
 	headerLogo.classList.remove('header__logo-container--open');
 
-  subNavigationOpen = false;
+	subNavigationOpen = false;
 }
 
 // Move the navigation
 function slide(direction) {
-  nav.classList.remove(
-    'navigation--slide-up',
-    'navigation--slide-down',
-    'navigation--slide-left',
-    'navigation--slide-right'
-  );
-  nav.classList.add(`navigation--slide-${direction}`)
+	nav.classList.remove(
+		'navigation--slide-up',
+		'navigation--slide-down',
+		'navigation--slide-left',
+		'navigation--slide-right'
+	);
+	nav.classList.add(`navigation--slide-${direction}`)
 }
 
 // Sub-menu - Display
 function hideSubMenus() {
-  
-  let delayInMilliseconds = 400;
 
-  setTimeout(function () {
-    for (let i = 0; i < navSubMenu.length; i++) {
-      navSubMenu[i].classList.add('hide');
-    };
+  	setTimeout(function () {
+		for (let i = 0; i < navSubMenu.length; i++) {
+			navSubMenu[i].classList.add('hide');
+		};
   }, delayInMilliseconds);  
 } 
 
 // Main-menu - Display
 function displayMainMenu() {
 
-  if(subNavigationOpen) {
-    navMainMenu.classList.remove('hide');   
-  } else {
-  let delayInMilliseconds = 400;
-
-  setTimeout(function () {
-    navMainMenu.classList.add('hide');   
-  }, delayInMilliseconds);  
-  }
-
+  	if(subNavigationOpen) {
+    	navMainMenu.classList.remove('hide');   
+  	} else {
+  	setTimeout(function () {
+    	navMainMenu.classList.add('hide');   
+  	}, delayInMilliseconds);  
+  	}
 } 
 
+// Switch hamburger menu/x-button
 function toggleMainMenuButton() {
-  if(mainNavigationOpen) {
-    mainMenuButton.classList.remove('header__menu-button--open');
-  } else {
-    mainMenuButton.classList.add('header__menu-button--open');
-  }
+	if(mainNavigationOpen) {
+		mainMenuButton.classList.remove('header__menu-button--open');
+	} else {
+		mainMenuButton.classList.add('header__menu-button--open');
+	}
 }
 
+// Switch logo/Back-button
 function toggleSubMenuButton() {
-  if(subNavigationOpen) {
-    backButton.classList.remove('header__back-button--open');
-    headerLogo.classList.remove('header__logo-container--open');
-  } else {
-    backButton.classList.toggle('header__back-button--open');
-    headerLogo.classList.toggle('header__logo-container--open');
-  }
+	if(subNavigationOpen) {
+		backButton.classList.remove('header__back-button--open');
+		headerLogo.classList.remove('header__logo-container--open');
+	} else {
+		backButton.classList.add('header__back-button--open');
+		headerLogo.classList.add('header__logo-container--open');
+	}
+}
+
+// Display Current Sub Menu 
+function displayCurrentSubMenu (event) {
+	let currentSubMenu = event.target.innerText.toLowerCase();
+	document.getElementById(`${currentSubMenu}`).classList.remove('hide');
 }
 
 // Open/close main menu
 function mainMenuButtonClickHandler(event) {
-  if (mainNavigationOpen) {
-    slide('up');
-    init();
-  }
-  else {
-    slide('down');
-  }
+	if (mainNavigationOpen) {
+		slide('up');
+		MobileNavigationInit();
+	}
+	else {
+		slide('down');
+	}
 
-  toggleMainMenuButton()
-  mainNavigationOpen = !mainNavigationOpen;
-  console.log(mainNavigationOpen);
+	toggleMainMenuButton()
+	mainNavigationOpen = !mainNavigationOpen;
 }
 
 // Open/clos sub menu
 function subMenuButtonsClickHandler(event) {
-  if (subNavigationOpen) {
-    hideSubMenus()
-    slide('right');
-    displayMainMenu()
-  }
-  else {
-    displaySubCurrentMenu(event)
-    displayMainMenu()
-    slide('left');
-  }
-
-  toggleSubMenuButton();
-  subNavigationOpen = !subNavigationOpen;
-  console.log(subNavigationOpen);
+	if (subNavigationOpen) {
+		slide('right');
+		hideSubMenus()
+	}
+	else {
+		displayCurrentSubMenu(event)
+		slide('left');
+	}
+	
+	displayMainMenu()
+	toggleSubMenuButton();
+	subNavigationOpen = !subNavigationOpen;
 }
-
-// Display Sub Menu 
-function displaySubCurrentMenu (event) {
-  let currentSubMenu = event.target.innerText.toLowerCase();
-  document.getElementById(`${currentSubMenu}`).classList.remove('hide');
-}
-
 
 // Eventlisteners
 mainMenuButton.addEventListener('click', mainMenuButtonClickHandler);
 
 for (let i = 0; i < subMenuButtons.length; i++) {
-  subMenuButtons[i].addEventListener('click', subMenuButtonsClickHandler)
+	subMenuButtons[i].addEventListener('click', subMenuButtonsClickHandler);
 }
 
 backButton.addEventListener('click', subMenuButtonsClickHandler)
