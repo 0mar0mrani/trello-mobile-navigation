374bf2307a37f383c35d8ac075a968d6a0cc3463
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2022-10-04 15:37:09 +0200

    Added animation to logo, back button

--- js/script.js
+++ js/script.js
@@ -28,8 +28,10 @@ function slide(direction) {
   nav.classList.add(`navigation--slide-${direction}`)
 }
 
-function toggleMainMenuButton() {
+function toggleAllButtonAnimations() {
   mainMenuButton.classList.toggle('header__menu-button--open');
+  backButton.classList.toggle('header__back-button--open');
+	headerLogo.classList.toggle('header__logo-container--open');
 }
 
 // Open main menu
@@ -41,7 +43,7 @@ function mainMenuButtonClickHandler(event) {
     slide('down');
   }
 
-  toggleMainMenuButton()
+  toggleAllButtonAnimations()
   navigationExpanded = !navigationExpanded;
 }
 
@@ -51,129 +53,3 @@ mainMenuButton.addEventListener('click', mainMenuButtonClickHandler);
 /****************/
 // Other
 const mobileNavigationWidth = 990;
-
-
-// // Make navigation ready for desktop view
-// const resetMenu = function () {
-//   document.getElementById('navigation__main').style.display = 'unset';
-
-//   for (let i = 0; i < buttonList.length; i++) {
-//     let currentButton = buttonList[i].innerText.toLowerCase();
-
-//     document.getElementById(`${currentButton}`).style.display = 'none';
-//   }
-
-//   document.getElementById('navigation__main').style.display = 'unset';
-// };
-
-// const resetMenuDesktop = function () {
-//   for (let i = 0; i < buttonList.length; i++) {
-//     let currentButton = buttonList[i].innerText.toLowerCase();
-//     document.getElementById(`${currentButton}`).style.display = 'none';
-//   }
-
-//   document.getElementById('navigation__main').style.display = 'flex';
-// };
-
-// // Check width when resized
-// const updateNavBar = function () {
-//   if (window.innerWidth > mobileNavigationWidth) {
-//     resetMenuDesktop();
-//   } else {
-//     resetMenu();
-//   }
-// };
-
-// updateNavBar();
-
-// window.addEventListener('resize', updateNavBar);
-
-// // Open Mobile Navigation
-// mainMenuButton.addEventListener('click', function () {
-//   if (nav.classList.contains('navigation--slide-up')) {
-//     slideDown();
-//   } else if (
-//     nav.classList.contains('navigation--slide-down') ||
-//     nav.classList.contains('navigation--slide-left') ||
-//     nav.classList.contains('navigation--slide-right')
-//   ) {
-//     slideUp();
-//   } else {
-//     slideDown();
-//   }
-
-//   // Swap back-button to Trello logo
-//   backButton.classList.remove('header__back-button--open');
-//   headerLogo.classList.remove('header__logo-container--open');
-
-//   // Animation on burger menu
-//   mainMenuButton.classList.toggle('header__menu-button--open');
-
-//   // Make body not scrollable
-//   body.classList.toggle('body--no-scroll');
-
-//   resetMenu();
-// });
-
-// // Allows for clicking in navigation to get to sub menus
-
-// let latestCurrentButton;
-
-// buttonList.forEach(function (i) {
-//   i.addEventListener('click', function (e) {
-//     // Get text from button clicked
-//     let currentButton = e.target.innerText.toLowerCase();
-//     console.log(currentButton);
-
-//     if (currentButton === latestCurrentButton) {
-//       document.getElementById(`${currentButton}`).style.display = 'none';
-//     } else {
-//       // Reset all
-//       if (window.innerWidth <= mobileNavigationWidth) {
-//         resetMenu();
-//       } else {
-//         resetMenuDesktop();
-//       }
-
-//       // Bring sub menu to front and display = unset
-//       document.getElementById(`${currentButton}`).style.display = 'unset';
-
-//       console.log(currentButton);
-
-//       currentButton = latestCurrentButton;
-
-//       if (window.innerWidth <= mobileNavigationWidth) {
-//         backButton.classList.add('header__back-button--open');
-//         headerLogo.classList.add('header__logo-container--open');
-
-//         // Transition main menu to left
-//         slideLeft();
-
-//         // Navigation main = removed after .3 s
-//         let delayInMilliseconds = 400;
-//         setTimeout(function () {
-//           document.getElementById('navigation__main').style.display = 'none';
-//         }, delayInMilliseconds);
-//       }
-//     }
-//   });
-// });
-
-// // Go back in mobile navigation
-// backButton.addEventListener('click', function (i) {
-//   backButton.classList.remove('header__back-button--open');
-//   headerLogo.classList.remove('header__logo-container--open');
-
-//   // Transition main menu to left
-//   nav.classList.remove('navigation--slide-left');
-//   nav.classList.add('navigation--slide-right');
-
-//   // Get back Navigation Main
-//   document.getElementById('navigation__main').style.display = 'block';
-
-//   // Navigation main = removed after .3 s
-//   let delayInMilliseconds = 400;
-//   setTimeout(function () {
-//     resetMenu();
-//   }, delayInMilliseconds);
-// });
